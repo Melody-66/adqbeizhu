3728d627ae9661cd11ee2da65337bbd298ff8fd3
Melody-66 <69851493+Melody-66@users.noreply.github.com>
2025-10-13 20:07:24 +0800

    Update adqbeizhu.user.js

--- adqbeizhu.user.js
+++ adqbeizhu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        ADQ账户备注显示（修复版）
 // @namespace    http://tampermonkey.net/
-// @version      0.3.4
+// @version      0.3.5
 // @description  根据页面特定元素内容修改网页标题
 // @author       Melody-66
 // @match        https://ad.qq.com/*
@@ -211,6 +211,7 @@
         setTimeout(init, 1000);
     }
 })();
+
 
 
 
